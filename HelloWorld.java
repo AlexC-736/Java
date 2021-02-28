6be82fdbe089bd8eed7420360f6531085c1c777f
AlexC-736 <60754419+AlexC-736@users.noreply.github.com>
2021-02-28 15:16:08 -0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+// Hello World File
+public class HelloWorld{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ HourGlass.java
@@ -0,0 +1,15 @@
+// Print an HourGlass with loops
+public class HourGlass{
+    public static void main(String[] args) {
+        for
+    }
+
+    public static String getLine (int lineNumber){
+        if (lineNumber == 1 || lineNumber == 5)
+            return "+---+";
+        else if (lineNumber == 2 || lineNumber == 4)
+            return " +-+ ";
+        else 
+            return "  +  ";
+    }
+}
